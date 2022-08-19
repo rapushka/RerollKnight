83def03fc63e432da12a9a68c67f2ae9127f9fae
Rapuška <megarubka44@gmail.com>
2022-08-19 10:12:38 +0300

    Generated GravityScaleComponent

---
+++ Assets/Code/Ecs/Components/GameComponents.cs
@@ -0,0 +1,7 @@
+using Code.WorkFlow.ComponentsTemplates;
+using Entitas.CodeGeneration.Attributes;
+
+namespace Code.Ecs.Components
+{
+	[Game] [Unique] public sealed class GravityScaleComponent : ValueComponent<float> { }
+}
--- Assets/Packages/Entitas/Generated/Game/GameComponentsLookup.cs
+++ Assets/Packages/Entitas/Generated/Game/GameComponentsLookup.cs
@@ -8,15 +8,15 @@
 //------------------------------------------------------------------------------
 public static class GameComponentsLookup {
 
+    public const int GravityScale = 0;
 
-
-    public const int TotalComponents = 0;
+    public const int TotalComponents = 1;
 
     public static readonly string[] componentNames = {
-
+        "GravityScale"
     };
 
     public static readonly System.Type[] componentTypes = {
-
+        typeof(Code.Ecs.Components.GravityScaleComponent)
     };
 }
