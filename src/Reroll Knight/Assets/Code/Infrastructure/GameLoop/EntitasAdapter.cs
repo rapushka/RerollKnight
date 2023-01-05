ffbbe8a2069c11cc9942bb0e7422f58e596299a3
Rapuška <mikalajsiatkouski@gmail.com>
2023-01-05 15:28:32 +0300

    +EntitasAdapter

---
+++ src/Reroll Knight/Assets/Code/Infrastructure/GameLoop/EntitasAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code
+{
+	public class EntitasAdapter : MonoBehaviour
+	{
+		private GameFeature _systems;
+
+		private void Start()
+		{
+			_systems = new GameFeature(Contexts.sharedInstance);
+			_systems.Initialize();
+		}
+
+		private void Update()
+		{
+			_systems.Execute();
+			_systems.Cleanup();
+		}
+
+		private void OnDestroy() => _systems.TearDown();
+	}
+}
