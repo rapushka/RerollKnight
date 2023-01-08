057687519f99322b13240fd3ce199973064a9789
rapushka <mikalajsiatkouski@gmail.com>
2023-01-08 16:03:32 +0300

    Adapter for Fixed Update

--- src/RerollKnight/Assets/Code/Infrastructure/UnityAdapter/EntitasAdapterBase.cs
+++ src/RerollKnight/Assets/Code/Infrastructure/UnityAdapter/EntitasAdapterBase.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Code
@@ -5,13 +6,17 @@ namespace Code
 	public abstract class EntitasAdapterBase : MonoBehaviour
 	{
 		private Feature _systems;
+		[CanBeNull] private Feature _fixedUpdateSystems;
 
-		protected Contexts Contexts => Contexts.sharedInstance;
+		protected static Contexts Contexts => Contexts.sharedInstance;
 
 		private void Start()
 		{
 			_systems = GetFeature();
+			_fixedUpdateSystems = GetFeatureForFixedUpdate();
+
 			_systems.Initialize();
+			_fixedUpdateSystems?.Initialize();
 		}
 
 		private void Update()
@@ -20,8 +25,20 @@ namespace Code
 			_systems.Cleanup();
 		}
 
-		private void OnDestroy() => _systems.TearDown();
+		private void FixedUpdate()
+		{
+			_fixedUpdateSystems?.Execute();
+			_fixedUpdateSystems?.Cleanup();
+		}
+
+		private void OnDestroy()
+		{
+			_systems.TearDown();
+			_fixedUpdateSystems?.TearDown();
+		}
 
 		protected abstract Feature GetFeature();
+
+		protected virtual Feature GetFeatureForFixedUpdate() => null;
 	}
 }
