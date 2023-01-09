52b266ee4e83a1d9819e70f666b121c4fd8d2176
rapushka <mikalajsiatkouski@gmail.com>
2023-01-10 02:48:29 +0300

    multy-fields components suppot

--- src/RerollKnight/Assets/Code/CodeGeneration/Plugins/AuthorityGenerator.cs
+++ src/RerollKnight/Assets/Code/CodeGeneration/Plugins/AuthorityGenerator.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using DesperateDevs.CodeGeneration;
+using DesperateDevs.Utils;
 using Entitas.CodeGeneration.Plugins;
 
 namespace Code
@@ -27,13 +30,12 @@ namespace Code
 
 			var fileContent = IsFlag(data)
 				? Template.FlagComponent(componentName, data.Context)
-				: Template.ValuableComponent(componentName, data.Context, data.MemberData.Single().type);
+				: Template.ValuableComponent(componentName, data.Context, data.MemberData);
 
 			return new CodeGenFile(fileName, fileContent, generatorName);
 		}
 
-		private static bool IsFlag(AuthorityData data) 
-			=> data.MemberData.ToArray().Any() == false;
+		private static bool IsFlag(AuthorityData data) => data.MemberData.ToArray().Any() == false;
 
 		private static class Template
 		{
@@ -54,19 +56,34 @@ public class {ClassName(component)} : {context}RegistrarBase
 }}
 ";
 
-			public static string ValuableComponent(string component, string context, string type)
+			public static string ValuableComponent(string component, string context, MemberData[] data)
 				=> $@"
 using UnityEngine;
 
 public class {ClassName(component)} : {context}RegistrarBase
 {{
-	[SerializeField] private {type} _value;
+{GenerateFields(data)}
 
-	public override void Register(ref {context}Entity entity) => entity.Add{component}(_value);
+	public override void Register(ref {context}Entity entity) => entity.Add{component}({GenerateArgs(data)});
 }}
 ";
 
 			public static string ClassName(string component) => $"{component}Authoring";
+
+			private static string GenerateFields(IEnumerable<MemberData> data) => Generate(data, "\n", AsField);
+
+			private static string GenerateArgs(IEnumerable<MemberData> data) => Generate(data, ", ", AsCamelCase);
+
+			private static string Generate
+				(IEnumerable<MemberData> data, string separator, Func<MemberData, string> template)
+				=> string.Join(separator, data.Select(template.Invoke).ToArray());
+
+			private static string AsField(MemberData member) => FieldTemplate(member.name, member.type);
+
+			private static string AsCamelCase(MemberData member) => member.name.LowercaseFirst();
+
+			private static string FieldTemplate(string name, string type)
+				=> $"\t[SerializeField] private {type} _{name};";
 		}
 	}
 }
