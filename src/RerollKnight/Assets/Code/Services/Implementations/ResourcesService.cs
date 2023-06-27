4814fd42aed1d387ad1adb71b868dadd7d86a2da
rapushka <mikalajsiatkouski@gmail.com>
2023-06-27 12:27:41 +0300

    +cell prefab

--- src/RerollKnight/Assets/Code/Services/Implementations/ResourcesService.cs
+++ src/RerollKnight/Assets/Code/Services/Implementations/ResourcesService.cs
@@ -5,10 +5,12 @@ namespace Code
 	public interface IResourcesService
 	{
 		GameEntityBehaviour PlayerPrefab { get; }
+		GameEntityBehaviour CellPrefab   { get; }
 	}
-	
+
 	public class ResourcesService : IResourcesService
 	{
 		public GameEntityBehaviour PlayerPrefab => Resources.Load<GameEntityBehaviour>("Player/Prefab");
+		public GameEntityBehaviour CellPrefab   => Resources.Load<GameEntityBehaviour>("Field/CellPrefab");
 	}
 }
