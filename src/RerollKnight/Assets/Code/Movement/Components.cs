7d4a7b204fab3f1da7e8eab208dc2ffa65496077
rapushka <mikalajsiatkouski@gmail.com>
2023-06-28 15:48:33 +0300

    click receiver added to cell prefab

--- src/RerollKnight/Assets/Code/Movement/Components.cs
+++ src/RerollKnight/Assets/Code/Movement/Components.cs
@@ -6,10 +6,10 @@ using static Entitas.CodeGeneration.Attributes.EventTarget;
 namespace Code
 {
 	[Game] [Behaviour] public sealed class InitialPositionComponent : IComponent { public UnityEngine.Vector3 Value; }
-	
+
 	[Game] public sealed class DestinationPositionComponent : IComponent { public UnityEngine.Vector3 Value; }
-	
+
 	[Game] [Behaviour] public sealed class MovingSpeedComponent : IComponent { public float Value; }
-	
+
 	[Game] [Event(Self)] public sealed class PositionComponent : IComponent { public UnityEngine.Vector3 Value; }
 }
