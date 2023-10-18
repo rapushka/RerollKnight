60da3c350a8fcc9ad03729279b5988fc37d88eb1
rapushka <mikalajsiatkouski@gmail.com>
2023-10-19 00:52:42 +0300

    minor style changes

--- src/RerollKnight/Assets/Code/Chips/Editor/TargetConstraintComponentBehaviourEditor.cs
+++ src/RerollKnight/Assets/Code/Chips/Editor/TargetConstraintComponentBehaviourEditor.cs
@@ -7,7 +7,7 @@ namespace Code.Editor
 	[CustomEditor(typeof(TargetConstraintsComponentBehaviour))]
 	public class TargetConstraintsComponentBehaviourEditor : UnityEditor.Editor
 	{
-		private ReorderableList _valueList;
+		private ReorderableList _values;
 		private string[] _componentNames;
 
 		private void OnEnable()
@@ -15,7 +15,7 @@ namespace Code.Editor
 			_componentNames = GameComponentsLookup.componentNames;
 
 			var constraints = serializedObject.FindProperty("_value");
-			_valueList = new ReorderableList(serializedObject, constraints, true, true, true, true)
+			_values = new ReorderableList(serializedObject, constraints, true, true, true, true)
 			{
 				drawHeaderCallback = DrawHeader,
 				drawElementCallback = DrawElement,
@@ -27,7 +27,6 @@ namespace Code.Editor
 		{
 			EditorGUILayout.LabelField("Target Constraints");
 
-			// Apply custom style changes
 			var customStyle = new GUIStyle(EditorStyles.helpBox)
 			{
 				padding = new RectOffset(10, 10, 10, 10),
@@ -35,13 +34,11 @@ namespace Code.Editor
 			};
 
 			EditorGUILayout.BeginVertical(customStyle);
-
-			serializedObject.Update();
-
-			_valueList.DoLayoutList();
-
-			serializedObject.ApplyModifiedProperties();
-
+			{
+				serializedObject.Update();
+				_values.DoLayoutList();
+				serializedObject.ApplyModifiedProperties();
+			}
 			EditorGUILayout.EndVertical();
 		}
 
@@ -52,25 +49,14 @@ namespace Code.Editor
 
 		private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
 		{
-			var elementProperty = _valueList.serializedProperty.GetArrayElementAtIndex(index);
-
 			rect.y += 2;
 
-			var selectedValue = elementProperty.intValue;
+			var elementProperty = _values.serializedProperty.GetArrayElementAtIndex(index);
+			rect = new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight);
 
-			// Find the index of the selected value in the component names array
-			var selectedIndex = GetSelectedIndex(selectedValue);
+			var selectedIndex = EditorGUI.Popup(rect, elementProperty.intValue, _componentNames);
 
-			// Display the dropdown with component names as options
-			selectedIndex = EditorGUI.Popup
-			(
-				new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight),
-				selectedIndex,
-				_componentNames
-			);
-
-			// Update the selected value in the list
-			elementProperty.intValue = GetSelectedValue(selectedIndex);
+			elementProperty.intValue = Clamp(selectedIndex);
 		}
 
 		private void AddItem(ReorderableList list)
@@ -80,24 +66,6 @@ namespace Code.Editor
 			serializedObject.ApplyModifiedProperties();
 		}
 
-		private int GetSelectedIndex(int selectedValue)
-		{
-			for (var i = 0; i < _componentNames.Length; i++)
-			{
-				if (_componentNames[i] == _componentNames[selectedValue])
-				{
-					return i;
-				}
-			}
-
-			return 0;
-		}
-
-		private int GetSelectedValue(int selectedIndex)
-		{
-			return selectedIndex >= 0 && selectedIndex < _componentNames.Length
-				? selectedIndex
-				: 0;
-		}
+		private int Clamp(int selectedIndex) => selectedIndex.Clamp(min: 0, max: _componentNames.Length);
 	}
 }
