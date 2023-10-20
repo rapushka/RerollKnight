d52e4d7640b27811521e546519b20589e3b3da1d
rapushka <mikalajsiatkouski@gmail.com>
2023-10-20 19:00:14 +0300

    on turn end - also unpick all

--- src/RerollKnight/Assets/Code/Chips/EndTurn/EndTurnSystem.cs
+++ src/RerollKnight/Assets/Code/Chips/EndTurn/EndTurnSystem.cs
@@ -1,14 +1,28 @@
 using Entitas;
-using static Code.GameState;
 
 namespace Code
 {
 	public sealed class EndTurnSystem : IExecuteSystem
 	{
 		private readonly Contexts _contexts;
+		private readonly IGroup<GameEntity> _targets;
 
-		public EndTurnSystem(Contexts contexts) => _contexts = contexts;
+		public EndTurnSystem(Contexts contexts)
+		{
+			_contexts = contexts;
+			_targets = _contexts.game.GetGroup(GameMatcher.PickedTarget);
+		}
 
-		public void Execute() => _contexts.TransferGameState(from: WaitingForAbilityUsage, to: TurnEnded);
+		public void Execute()
+		{
+			if (!_contexts.GameStateIs(GameState.WaitingForAbilityUsage))
+				return;
+
+			_contexts.ToGameState(GameState.TurnEnded);
+			_contexts.game.pickedChipEntity.Unpick();
+
+			foreach (var target in _targets.GetEntities())
+				target.Unpick();
+		}
 	}
 }
