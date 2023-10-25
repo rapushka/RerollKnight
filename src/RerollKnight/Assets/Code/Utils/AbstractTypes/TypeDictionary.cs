be1b4cb24c28b45ede45062af9eb44a2474b9d5d
rapushka <mikalajsiatkouski@gmail.com>
2023-10-26 02:56:58 +0300

    ensure empty TypeDictionary

--- src/RerollKnight/Assets/Code/Utils/AbstractTypes/TypeDictionary.cs
+++ src/RerollKnight/Assets/Code/Utils/AbstractTypes/TypeDictionary.cs
@@ -5,7 +5,15 @@ namespace Code
 {
 	public class TypeDictionary<TBase> : Dictionary<Type, TBase>
 	{
-		public void Add(TBase value) => this[value.GetType()] = value;
+		public void Add(TBase value)
+		{
+			var type = value.GetType();
+
+			if (ContainsKey(type))
+				throw new Exception($"Dictionary already have value of the type {type.Name}!");
+
+			this[type] = value;
+		}
 
 		public T Get<T>() where T : TBase => (T)this[typeof(T)];
 
