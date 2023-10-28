a5b8304f9525345c12e68d85261321e6e4a7e2f9
rapushka <mikalajsiatkouski@gmail.com>
2023-10-29 02:05:17 +0300

    field's cells are targets now

--- src/RerollKnight/Assets/Code/Outline/Components.cs
+++ src/RerollKnight/Assets/Code/Outline/Components.cs
@@ -1,3 +1,4 @@
+using Code.CodeGeneration.Attributes;
 using Entitas;
 using Entitas.CodeGeneration.Attributes;
 using static Entitas.CodeGeneration.Attributes.EventTarget;
@@ -7,7 +8,7 @@ namespace Code
 	[Game] [Event(Self)] public sealed class OutlineComponent : IComponent { public OutlineParams Value; }
 	
 	// can be a target for a chip usage. sometimes 
-	[Game] public sealed class TargetComponent : IComponent { }
+	[Game] [Behaviour] public sealed class TargetComponent : IComponent { }
 
 	// can be picked as a target. right now
 	[Game] public sealed class AvailableToPickComponent : IComponent { }
