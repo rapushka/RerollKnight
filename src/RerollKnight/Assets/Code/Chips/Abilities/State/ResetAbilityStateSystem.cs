11c02a4cf020fe5569b3cf7a789e2707edc71571
rapushka <mikalajsiatkouski@gmail.com>
2023-11-15 16:50:43 +0300

    ResetAbilityStateSystem is TearDown System now

--- src/RerollKnight/Assets/Code/Chips/Abilities/State/ResetAbilityStateSystem.cs
+++ src/RerollKnight/Assets/Code/Chips/Abilities/State/ResetAbilityStateSystem.cs
@@ -4,14 +4,14 @@ using static Entitas.Generic.ScopeMatcher<Code.ChipsScope>;
 
 namespace Code
 {
-	public sealed class ResetAbilityStateSystem : ICleanupSystem
+	public sealed class ResetAbilityStateSystem : ITearDownSystem
 	{
 		private readonly IGroup<Entity<ChipsScope>> _entities;
 
 		public ResetAbilityStateSystem(Contexts contexts)
 			=> _entities = contexts.Get<ChipsScope>().GetGroup(Get<Component.AbilityState>());
 
-		public void Cleanup()
+		public void TearDown()
 		{
 			foreach (var e in _entities.WhereStateIs(AbilityState.Casted))
 				e.Replace<Component.AbilityState, AbilityState>(AbilityState.Inactive);
