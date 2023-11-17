665f9561f66f5d216f0265f2b7d789d6531e0ffe
rapushka <mikalajsiatkouski@gmail.com>
2023-11-17 18:52:45 +0300

    debug enemy's turn

--- src/RerollKnight/Assets/Code/GameStates/StateMachine/States/OtherPlayerTurn/OtherPlayerTurnGameplayState.cs
+++ src/RerollKnight/Assets/Code/GameStates/StateMachine/States/OtherPlayerTurn/OtherPlayerTurnGameplayState.cs
@@ -1,3 +1,6 @@
+using Entitas;
+using UnityEngine;
+
 namespace Code
 {
 	public class OtherPlayerTurnGameplayState : GameplayStateBase<OtherPlayerTurnGameplayState.StateFeature>
@@ -10,8 +13,17 @@ namespace Code
 				: base($"{nameof(OtherPlayerTurnGameplayState)}.{nameof(StateFeature)}", factory)
 			{
 				// Initialize
+				Add<SayHiSystem>();
 				Add<ToGameplayStateSystem<ObservingGameplayState>>();
 			}
+		}
+	}
+
+	public class SayHiSystem : IInitializeSystem
+	{
+		public void Initialize()
+		{
+			Debug.Log("it's enemy's turn!");
 		}
 	}
 }
