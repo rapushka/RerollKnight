24db8ef1d2dac1c4f2e3329f1a4c68c481437daa
rapushka <mikalajsiatkouski@gmail.com>
2023-12-03 21:55:37 +0300

    +kick config

--- src/RerollKnight/Assets/Code/Chips/Abilities/Components.cs
+++ src/RerollKnight/Assets/Code/Chips/Abilities/Components.cs
@@ -6,6 +6,8 @@ namespace Code.Component
 
 	[ChipsScope] public sealed class SwitchPositions : FlagComponent { }
 
+	[ChipsScope] public sealed class Kick : FlagComponent { }
+
 	[ChipsScope] public sealed class MaxCountOfTargets : ValueComponent<int> { }
 
 	[ChipsScope] public sealed class Range : ValueComponent<int> { }
