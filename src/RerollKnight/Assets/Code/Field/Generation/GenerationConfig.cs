dec12d7d737e56574a7905a37de3293d7f13e95c
rapushka <mikalajsiatkouski@gmail.com>
2023-12-06 23:02:19 +0300

    Field Sizes moved to Generation Config

--- src/RerollKnight/Assets/Code/Field/Generation/GenerationConfig.cs
+++ src/RerollKnight/Assets/Code/Field/Generation/GenerationConfig.cs
@@ -6,6 +6,7 @@ namespace Code
 	[CreateAssetMenu(fileName = "Generation Config", menuName = ToolPath.Root + "Generation Config", order = 0)]
 	public class GenerationConfig : ScriptableObject
 	{
-		[field: SerializeField] public RangeInt EnemiesCount { get; private set; }
+		[field: SerializeField] public RangeInt    EnemiesCount { get; private set; }
+		[field: SerializeField] public Coordinates FieldSizes   { get; private set; }
 	}
 }
--- src/RerollKnight/Assets/Code/Field/SpawnFieldSystem.cs
+++ src/RerollKnight/Assets/Code/Field/SpawnFieldSystem.cs
@@ -9,34 +9,34 @@ namespace Code
 	{
 		private readonly IResourcesService _resources;
 		private readonly IAssetsService _assets;
-		private readonly ILayoutService _layout;
 		private readonly IHoldersProvider _holdersProvider;
+		private readonly GenerationConfig _generationConfig;
 
 		[Inject]
 		public SpawnFieldSystem
 		(
 			IResourcesService resources,
 			IAssetsService assets,
-			ILayoutService layout,
-			IHoldersProvider holdersProvider
+			IHoldersProvider holdersProvider,
+			GenerationConfig generationConfig
 		)
 		{
 			_resources = resources;
 			_assets = assets;
-			_layout = layout;
 			_holdersProvider = holdersProvider;
+			_generationConfig = generationConfig;
 		}
 
 		private EntityBehaviour<GameScope> CellPrefab => _resources.CellPrefab;
 
 		public void Initialize()
 		{
-			for (var x = 0; x < _layout.FieldSizes.Column; x++)
-			for (var y = 0; y < _layout.FieldSizes.Row; y++)
+			for (var x = 0; x < _generationConfig.FieldSizes.Column; x++)
+			for (var y = 0; y < _generationConfig.FieldSizes.Row; y++)
 			{
 				_assets.SpawnBehaviour(CellPrefab, _holdersProvider.CellsHolder.transform).Entity
-					.Add<CoordinatesUnderField, Coordinates>(new Coordinates(x, y))
-					.Is<Empty>(true)
+				       .Add<CoordinatesUnderField, Coordinates>(new Coordinates(x, y))
+				       .Is<Empty>(true)
 					;
 			}
 		}
