e7f3318bf5f00565dddbe46c1f58eec05b8ac15a
rapushka <mikalajsiatkouski@gmail.com>
2023-12-09 12:38:02 +0300

    Cleanup in View directory

--- src/RerollKnight/Assets/Code/View/Components.cs
+++ src/RerollKnight/Assets/Code/View/Components.cs
@@ -7,10 +7,6 @@ namespace Code.Component
 
 	[GameScope] public sealed class ViewTransform : ValueComponent<Transform> { }
 
-	[GameScope] public sealed class LookAt : ValueComponent<Entity<GameScope>>, IEvent<Self> { }
-
-	[GameScope] public sealed class Rotation : ValueComponent<Quaternion>, IEvent<Self> { }
-
 	[GameScope] public sealed class WorldSpaceUi : FlagComponent { }
 
 	[GameScope] public sealed class Camera : FlagComponent, IUnique { }
