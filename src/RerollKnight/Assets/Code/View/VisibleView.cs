58948b4c1e0431dd8e7917f7ea9a4e306bdb18e0
rapushka <mikalajsiatkouski@gmail.com>
2023-12-09 13:12:09 +0300

    temporary nice hidding/showing of chips

--- src/RerollKnight/Assets/Code/View/VisibleView.cs
+++ src/RerollKnight/Assets/Code/View/VisibleView.cs
@@ -9,6 +9,11 @@ namespace Code
 		[SerializeField] private GameObject _target;
 
 		public override void OnValueChanged(Entity<GameScope> entity, Visible component)
-			=> _target.SetActive(entity.Is<Visible>());
+		{
+			var position = entity.Get<Position>().Value;
+			entity.Replace<DestinationPosition, Vector3>(position.Set(y: -10)); // TODO: set from layout service
+
+			// _target.SetActive(entity.Is<Visible>());
+		}
 	}
 }
