d548095341d5f9495840f50de766004850734230
rapushka <mikalajsiatkouski@gmail.com>
2023-12-15 01:45:14 +0300

    +MainMenu

--- src/RerollKnight/Assets/Code/UI/UiMediator.cs
+++ src/RerollKnight/Assets/Code/UI/UiMediator.cs
@@ -1,6 +1,7 @@
 using Code.Component;
 using Entitas.Generic;
 using JetBrains.Annotations;
+using UnityEngine;
 using Zenject;
 
 namespace Code
@@ -21,5 +22,19 @@ namespace Code
 		public void EndTurn() => _contexts.Get<RequestScope>().CreateEntity().Add<EndTurn>();
 
 		public bool IsEndTurnButtonAvailable => CurrentActor?.Is<Player>() ?? false;
+
+		public void OpenGameplay() { }
+
+		public void OpenSettings() { }
+
+		public void Exit()
+		{
+#if UNITY_EDITOR
+			if (UnityEditor.EditorApplication.isPlaying)
+				UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+
+			Application.Quit();
+		}
 	}
 }
