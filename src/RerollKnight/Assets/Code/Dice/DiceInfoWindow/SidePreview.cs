c9cc42b23933e87f8bb19c7f75a643caf3824580
rapushka <mikalajsiatkouski@gmail.com>
2023-12-17 20:07:43 +0300

    fix chips overflow

--- src/RerollKnight/Assets/Code/Dice/DiceInfoWindow/SidePreview.cs
+++ src/RerollKnight/Assets/Code/Dice/DiceInfoWindow/SidePreview.cs
@@ -3,6 +3,7 @@ using Code.Component;
 using Entitas.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Code
 {
@@ -12,17 +13,28 @@ namespace Code
 		[SerializeField] private GameObject _isActiveLabel;
 		[SerializeField] private TMP_Text _sideNumberTextMesh;
 		[SerializeField] private ChipUiPreview _chipPreviewPrefab;
+		[SerializeField] private HorizontalLayoutGroup _chipsLayoutGroup;
+
+		[Header("View")]
+		[SerializeField] private int _chipsOverflowThreshold = 6;
+		[SerializeField] private float _overflowShorter = 30f;
 
 		public void SetData(Entity<GameScope> sideEntity)
 		{
 			_sideNumberTextMesh.text = sideEntity.Get<Face>().Value.ToString();
 			_isActiveLabel.SetActive(sideEntity.IsActiveFace());
+			var chipsCount = 0;
 
 			foreach (var chip in sideEntity.GetDependants().Where((e) => e.Is<Chip>()))
 			{
 				var chipPreview = Instantiate(_chipPreviewPrefab, _chipsRoot);
 				chipPreview.SetData(chip);
+				chipsCount++;
 			}
+
+			var chipsOverflow = chipsCount - _chipsOverflowThreshold;
+			if (chipsOverflow > 0)
+				_chipsLayoutGroup.spacing = chipsOverflow * -_overflowShorter;
 		}
 	}
 }
