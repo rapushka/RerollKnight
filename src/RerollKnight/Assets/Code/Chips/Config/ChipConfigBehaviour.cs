65f17906d344c80f0399e06328f4fb062a77acb3
rapushka <mikalajsiatkouski@gmail.com>
2023-12-17 13:31:46 +0300

    a little better properties order

--- src/RerollKnight/Assets/Code/Chips/Config/ChipConfigBehaviour.cs
+++ src/RerollKnight/Assets/Code/Chips/Config/ChipConfigBehaviour.cs
@@ -6,10 +6,10 @@ namespace Code
 {
 	public class ChipConfigBehaviour : MonoBehaviour
 	{
-		[field: SerializeField] public List<AbilityConfigBehaviour> Abilities { get; private set; }
-		[field: SerializeField] public float                        Cost      { get; private set; }
-
 		[field: SerializeField] public LocalizedString LabelKey       { get; private set; }
 		[field: SerializeField] public LocalizedString DescriptionKey { get; private set; }
+
+		[field: SerializeField] public float                        Cost      { get; private set; }
+		[field: SerializeField] public List<AbilityConfigBehaviour> Abilities { get; private set; }
 	}
 }
