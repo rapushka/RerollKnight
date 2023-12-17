a6c71e0cf9a3a10a37005e435a5c20e6cb9ee71f
rapushka <mikalajsiatkouski@gmail.com>
2023-12-17 12:57:48 +0300

    descriptions for cells

--- src/RerollKnight/Assets/Code/Chips/Config/ChipConfigBehaviour.cs
+++ src/RerollKnight/Assets/Code/Chips/Config/ChipConfigBehaviour.cs
@@ -8,5 +8,8 @@ namespace Code
 		[field: SerializeField] public string                       Label     { get; private set; }
 		[field: SerializeField] public List<AbilityConfigBehaviour> Abilities { get; private set; }
 		[field: SerializeField] public float                        Cost      { get; private set; }
+
+		[field: Multiline]
+		[field: SerializeField] public string Description { get; private set; }
 	}
 }
