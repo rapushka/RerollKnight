9268bd4defc81f49b1d1ba3d807cdc5bc25c1709
Kolia Siatkouski <n.siatkouski@furylion.dev>
2023-12-19 19:49:46 +0300

    Reworked Battle Log

--- src/RerollKnight/Assets/Code/UI/BattleLog/BattleLog.cs
+++ src/RerollKnight/Assets/Code/UI/BattleLog/BattleLog.cs
@@ -1,47 +1,29 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Code
 {
 	public class BattleLog : MonoBehaviour
 	{
-		private const float Spacing = 5f;
-
 		[SerializeField] private LogEntry _logEntryPrefab;
 		[SerializeField] private RectTransform _root;
+		[SerializeField] private int _maxCountOfLogs;
 
 		private readonly List<LogEntry> _showedLogs = new();
-		private float _entryHeight;
-		private float _wholeHeight;
-
-		// Tricky way to fade and have all entries fit into root
-		private float OpacityStep => 1 / ((_wholeHeight - _entryHeight) / (_entryHeight + Spacing));
-
-		private void Start()
-		{
-			_wholeHeight = _root.rect.height;
-			_entryHeight = _logEntryPrefab.GetComponent<RectTransform>().rect.height;
-		}
 
 		public void Log(string message)
 		{
-			DimShowedLogs();
 			_showedLogs.Add(NewLogEntry(message));
+
+			if (_showedLogs.Count > _maxCountOfLogs)
+				Remove(_showedLogs.First());
 		}
 
-		private void DimShowedLogs()
+		private void Remove(LogEntry log)
 		{
-			for (var i = _showedLogs.Count - 1; i >= 0; i--)
-			{
-				var log = _showedLogs[i];
-				log.Opacity -= OpacityStep;
-
-				if (log.Opacity <= 0f)
-				{
-					_showedLogs.Remove(log);
-					Destroy(log.gameObject);
-				}
-			}
+			_showedLogs.Remove(log);
+			Destroy(log.gameObject);
 		}
 
 		private LogEntry NewLogEntry(string message)
