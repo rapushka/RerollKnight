a9a7af8fb5b3ea34b331bcfadbb2fd05514dbec8
rapushka <mikalajsiatkouski@gmail.com>
2023-12-23 13:48:00 +0300

    Don't repick side with PredefineRandomSide

--- src/RerollKnight/Assets/Code/GameStates/StateMachine/States/GameLoop/Reroll/SidesRollingExtensions.cs
+++ src/RerollKnight/Assets/Code/GameStates/StateMachine/States/GameLoop/Reroll/SidesRollingExtensions.cs
@@ -7,6 +7,9 @@ namespace Code
 	{
 		public static void PredefineRandomSide(this Entity<GameScope> actor)
 		{
+			if (actor.Has<PredefinedNextSide>())
+				return;
+
 			var randomFace = actor.GetDependants().WhereHas<Face>().PickRandom();
 			actor.Add<PredefinedNextSide, int>(randomFace.Get<Face>().Value);
 		}
