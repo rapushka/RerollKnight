0b468c171d51d9fda7bb58b5e46982c14c2c822e
rapushka <mikalajsiatkouski@gmail.com>
2023-12-24 18:54:20 +0300

    Removed FormerlySerialized attribute

--- src/RerollKnight/Assets/Code/Chips/Config/ChipConfigBehaviour.cs
+++ src/RerollKnight/Assets/Code/Chips/Config/ChipConfigBehaviour.cs
@@ -8,7 +8,6 @@ namespace Code
 	{
 		[field: SerializeField] public LocalizedString LabelKey { get; private set; }
 
-		[field: UnityEngine.Serialization.FormerlySerializedAs("_cost")]
 		[field: SerializeField] public int Cost { get; private set; }
 
 		[field: SerializeField] public List<AbilityConfigBehaviour> Abilities { get; private set; }
