c92c2f357505d3e57669d467a11540d6c468fe7a
rapushka <mikalajsiatkouski@gmail.com>
2023-12-25 02:45:58 +0300

    Fix

--- src/RerollKnight/Assets/Code/AI/Systems/ChooseEnemyStrategySystem.cs
+++ src/RerollKnight/Assets/Code/AI/Systems/ChooseEnemyStrategySystem.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using Code.Component;
 using Entitas;
 using Entitas.Generic;
+using UnityEngine;
+using static Entitas.Generic.ScopeMatcher<Code.GameScope>;
 
 namespace Code
 {
@@ -11,24 +13,27 @@ namespace Code
 		private readonly Contexts _contexts;
 		private readonly ChipKinds _chipKinds;
 
+		private readonly IGroup<Entity<GameScope>> _chips;
+
 		public ChooseEnemyStrategySystem(Contexts contexts, ChipKinds chipKinds)
 		{
 			_contexts = contexts;
 			_chipKinds = chipKinds;
+
+			_chips = contexts.GetGroup(AllOf(Get<Chip>(), Get<AvailableToPick>()));
 		}
 
-		private bool HasAnyAttackingChip => Chips.Any(_chipKinds.IsAttackingChip);
-
-		private IEnumerable<Entity<GameScope>> Chips => CurrentActor.GetDependants().WhereHas<Chip>();
+		private bool HasAnyAttackingChip => _chips.Any(_chipKinds.IsAttackingChip);
 
 		private Entity<GameScope> CurrentActor => _contexts.Get<GameScope>().Unique.GetEntity<CurrentActor>();
 
 		public void Initialize()
 		{
-			var strategy = Chips.Any()
+			var strategy = _chips.Any()
 				? HasAnyAttackingChip ? EnemyStrategy.Attack : EnemyStrategy.Defence
 				: EnemyStrategy.EndTurn;
 
+			Debug.Log($"strategy = {strategy}");
 			CurrentActor.Replace<CurrentStrategy, EnemyStrategy>(strategy);
 		}
 	}
