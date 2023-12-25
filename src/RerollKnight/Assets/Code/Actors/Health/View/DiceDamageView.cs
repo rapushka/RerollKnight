2f38025e9882be56105afd0138ed2996be395145
rapushka <mikalajsiatkouski@gmail.com>
2023-12-25 21:24:17 +0300

    red outline on taking damage

--- src/RerollKnight/Assets/Code/Actors/Health/View/DiceDamageView.cs
+++ src/RerollKnight/Assets/Code/Actors/Health/View/DiceDamageView.cs
@@ -1,3 +1,4 @@
+using cakeslice;
 using Code.Component;
 using DG.Tweening;
 using Entitas.Generic;
@@ -8,18 +9,29 @@ namespace Code
 	public class DiceDamageView : BaseListener<GameScope, Health>
 	{
 		[SerializeField] private Transform _transform;
+		[SerializeField] private Outline _outline;
+
+		[Header("Tweaks")]
 		[SerializeField] private float _duration = 0.2f;
 		[SerializeField] private float _strength = 0.25f;
 		[SerializeField] private int _vibrato = 100;
 
-		private IViewConfig _viewConfig;
-
 		private int _previousHealth;
+		private Sequence _sequence;
 
 		public override void OnValueChanged(Entity<GameScope> entity, Health component)
 		{
 			if (component.Value < _previousHealth)
-				_transform.DOShakePosition(_duration, _strength, _vibrato);
+			{
+				_sequence?.Kill();
+
+				_outline.enabled = true;
+				_outline.color = (int)TargetState.Wrong;
+
+				_sequence = DOTween.Sequence()
+				                   .Append(_transform.DOShakePosition(_duration, _strength, _vibrato))
+				                   .AppendCallback(() => _outline.enabled = false);
+			}
 
 			_previousHealth = component.Value;
 		}
