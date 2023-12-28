00195f2d4d83614d8b38d166ed3127142aef3f53
Kolia Siatkouski <n.siatkouski@furylion.dev>
2023-12-28 19:30:45 +0300

    +Loading screen

---
+++ src/RerollKnight/Assets/Code/UI/LoadingCurtain/DoTweenExtensions.cs
@@ -0,0 +1,7 @@
+namespace Code
+{
+	public static class DoTweenExtensions
+	{
+		public static float ToFloat(this bool @this) => @this ? 1f : 0f;
+	}
+}
---
+++ src/RerollKnight/Assets/Code/UI/LoadingCurtain/LoadingCurtain.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Code
+{
+	public class LoadingCurtain : WindowBase
+	{
+		[SerializeField] private CanvasGroup _canvasGroup;
+		[SerializeField] private float _duration;
+
+		protected override void OnShow()
+		{
+			ShowImmediately();
+		}
+
+		public void FadeIn() => SetFade(true, _duration);
+
+		public void FadeOut() => SetFade(false, _duration);
+
+		public void ShowImmediately() => SetFade(true, 0f);
+
+		public void HideImmediately() => SetFade(false, 0f);
+
+		private void SetFade(bool value, float duration) => _canvasGroup.DOFade(value.ToFloat(), duration);
+	}
+}
